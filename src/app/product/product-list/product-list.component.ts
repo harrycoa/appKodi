70042d027bdc7e4f20060b960454d901e7171bed
Harry Coa <rodol_2000@hotmail.com>
2020-05-09 10:24:11 -0500

    ngif ngfor databinding

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -3,25 +3,50 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+  productTitle: string = 'Lista de Productos';
+  imageWidth: number = 50;
+  imageMArgin: number = 2;
+  showImage: boolean = false;
+  /* mensajeBoton: string = 'Ocultar imagen';
+  mensajeBoton2: string = 'Mostrar imagen'; */
 
-  productTitle:string = 'Lista de Productos';
-  product:any[] = [
+  products: any[] = [
     {
-      "productoId": 1,
-      "productName": 'Camisa volcom'
+      productoId: 1,
+      productName: 'Camisa volcom',
+      description: 'camisa 100 % algodon',
+      createDate: '21/05/2020',
+      price: '500',
+      rating: '2.6',
+      imageUrl: 'https://www.skatepro.es/99-35448.htm#show_image',
     },
-    {}
-
-
-
+    {
+      productoId: 2,
+      productName: 'Camisa volcom2',
+      description: 'camisa 100 % algodon',
+      createDate: '21/05/2020',
+      price: '500',
+      rating: '2.6',
+      imageUrl: 'https://www.skatepro.es/99-35342.htm#show_image',
+    },
+    {
+      productoId: 3,
+      productName: 'Camisa volcom3',
+      description: 'camisa 100 % algodon',
+      createDate: '21/05/2020',
+      price: '500',
+      rating: '2.6',
+      imageUrl: 'https://www.skatepro.es/99-35342.htm#show_image',
+    },
   ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ShowImage(): void {
+    this.showImage = !this.showImage;
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
